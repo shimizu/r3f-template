e6f12ecd2548fa60074659807a1ab172ee48e317
shimizu <xxshimizuxx@gmail.com>
2025-12-09 16:30:14 +0900

    feat: src/theatre/state.json の読み込みを追加

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import App from './App.jsx'
 import studio from '@theatre/studio'
 import extension from '@theatre/r3f/dist/extension'
 import { getProject } from '@theatre/core'
+import demoProjectState from './theatre/state.json' // state.jsonをインポート
 
 if (import.meta.env.DEV) {
   studio.initialize()
   studio.extend(extension)
 }
 
-const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+const demoSheet = getProject('Demo Project', { state: demoProjectState }).sheet('Demo Sheet') // stateオプションとして渡す
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
